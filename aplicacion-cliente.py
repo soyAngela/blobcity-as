7ced72aff2120a7a9367e0a8f251928efcab373d
soyAngela <angelagonzalez1999@hotmail.es>
2022-11-24 23:19:21 +0100

    Update aplicacion-cliente.py

--- aplicacion-cliente.py
+++ aplicacion-cliente.py
@@ -8,32 +8,27 @@ while True:
   mensaje = input("Introduce el dato que quieres guardar en la bd: ")
   
 
-  url = 'http://localhost:10111/rest/bquery'
+  url = 'http://35.195.125.149:10111/rest/bquery'
 
   #Create datastore request
   datos = {
-    "key": "",
+    "key": "BC.9f8e21f3e73e4751ac5582b5edb52548e1cd029d26554c6eb5ec70b31f67e2fc",
     "username" : "root",
     "password" : "root",
     "ds" : "prueba",
     "c" : "coleccion",
     "q" : "insert",
-    "p" : { },
-    "data" : ["JSON", {
+    "p" : { "data" : ["JSON", {
         "nombre": nombre,
-        "mensaje": mensaje }]
+        "mensaje": mensaje }] },
+    
   }
 
   headers = {
     'Content-Type': 'application/json',
     }
 
-  response = requests.post(url=url, json=urllib.parse.urlencode(datos), headers=headers)
-
-  print(response)
-  print(response.headers)
-  print(response.text)
-  print(response.content)
+  response = requests.post(url=url, json=datos, headers=headers)
 
   with open('./log_blobcity.txt','a') as f:
     f.write(str(response))
@@ -41,3 +36,7 @@ while True:
     f.write(str(response.text))
     f.write('\n')
     f.write(str(response.content))
+    f.write('\n')
+    f.write(str(response.request.body))
+    f.write('\n')
+    f.write('\n')
