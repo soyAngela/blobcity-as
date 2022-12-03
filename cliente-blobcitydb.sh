08775de0d7a5e30e7c1224f68f558400fe9ec643
soyAngela <angelagonzalez1999@hotmail.es>
2022-12-03 21:09:29 +0100

    Obtener la contraseña

--- cliente-blobcitydb.sh
+++ cliente-blobcitydb.sh
@@ -1,4 +1,11 @@
 sleep 10
+
+container_id=$ (sudo docker ps -aqf "name=blobcity-db-container")
+password=$ (sudo docker exec -it $container_id cat /data/root-pass.txt)
+
+sed -i "2s/.*/$password/" comandos-blobcitydb.txt
+
 nc blobcity-db-container 10113 < comandos-blobcitydb.txt
+
 echo "Creada nueva base de datos: REGISTRO"
 echo "Creada nueva coleccion: VISITAS"
