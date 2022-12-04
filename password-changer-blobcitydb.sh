79f342153f051a8ab964a0f30b0fa15689f26249
soyAngela <angelagonzalez1999@hotmail.es>
2022-12-04 20:05:53 +0100

    Script cambio de contraseña

--- password-changer-blobcitydb.sh
+++ password-changer-blobcitydb.sh
@@ -1,8 +1,10 @@
 container_id=$(sudo docker ps -aqf "name=blobcity-db-container")
 password=$(sudo docker exec -it $container_id cat /data/root-pass.txt)
+password=$(echo $password%?})
 
-echo root > password-change.txt
-echo $password >> password-change.txt
+echo "root" > password-change.txt
+echo "$password" >> password-change.txt
 echo "set-user-password root $password root" >> password-change.txt
+echo "exit" >> password-change.txt
 
-nc blobcity-db-container 10113 < password-change.txt
+nc localhost 10113 < password-change.txt
